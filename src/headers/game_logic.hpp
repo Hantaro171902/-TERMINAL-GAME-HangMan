70b8b13ff4fcb18139712cc1207074bcb10d5859
Hantaro171902 <chaugiahan001@gmail.com>
2025-07-27 21:46:29 +0700

    update game_logic header

---
+++ src/headers/game_logic.hpp
@@ -0,0 +1,16 @@
+#pragma once;
+
+#include <string>
+#include <vector>
+
+enum GameState { s_playing = 0, s_won, s_lost, s_ended };
+enum GuessResult { t_waiting = 0, t_correct, t_incorrect, t_duplicate };
+
+void start_game(int min_letters, int max_letters, int given, WordTheme theme);
+void guess_char(char key);
+int guesses_incorrect();
+int guesses_correct();
+void refresh_status();
+void refresh_turn();
+void print_level();
+
